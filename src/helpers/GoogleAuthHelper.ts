a0a316ae98307d0c3ad81b90223bf26d4f0b5e68
flame11st <flame11.st@gmail.com>
2020-06-11 17:27:33 +0300

    Update GoogleAuthHelper.ts

--- src/helpers/GoogleAuthHelper.ts
+++ src/helpers/GoogleAuthHelper.ts
@@ -1,13 +1,19 @@
 declare const gapi: any;
 
+const key = "4993407912-dakflmq66sk3vshr6qnm3ld2cvjp6l44.apps.googleusercontent.com"; //PROD
+const cookiePolicy =  'https://moviediary.azurewebsites.net'; //PROD
+
+// const key = "4993407912-b5urkj1fodpa8b8ftftkeclt2q4q9v9d.apps.googleusercontent.com"; //DEV
+// const cookiePolicy =  'http://localhost:8080/'; //DEV
+
 class GoogleAuthHelper {
     auth2Instance: any;
 
     Init(button: Element, clickHandler: Function) {
         gapi.load('auth2', async () => {
             this.auth2Instance = await gapi.auth2.init({
-                client_id: '4993407912-b5urkj1fodpa8b8ftftkeclt2q4q9v9d.apps.googleusercontent.com',
-                cookiepolicy: 'http://localhost:8080/',
+                client_id: key,
+                cookiepolicy: cookiePolicy,
             });
 
             this.auth2Instance.attachClickHandler(button, {}, (googleUser: any) => clickHandler(googleUser));
@@ -18,8 +24,8 @@ class GoogleAuthHelper {
         if (!this.auth2Instance) {
             gapi.load('auth2', async () => {
                 this.auth2Instance = await gapi.auth2.init({
-                    client_id: '4993407912-b5urkj1fodpa8b8ftftkeclt2q4q9v9d.apps.googleusercontent.com',
-                    cookiepolicy: 'http://localhost:8080/',
+                    client_id: key,
+                    cookiepolicy: cookiePolicy,
                 });
 
                 this.auth2Instance.signIn().then((googleUser: any) => {
@@ -37,8 +43,8 @@ class GoogleAuthHelper {
         if (!this.auth2Instance) {
             await gapi.load('auth2', async () => {
                 this.auth2Instance = await gapi.auth2.init({
-                    client_id: '4993407912-b5urkj1fodpa8b8ftftkeclt2q4q9v9d.apps.googleusercontent.com',
-                    cookiepolicy: 'http://localhost:8080/',
+                    client_id: key,
+                    cookiepolicy: cookiePolicy,
                 });
 
                 this.auth2Instance.signOut();
