acfd02c7d986be586e7cba88af5402223bc2054b
flame11st <flame11.st@gmail.com>
2021-09-03 23:53:05 +0300

    03.09

---
+++ dist/precache-manifest.3d173e0300f8029916ae4144ef93cc48.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "837953fcac4d13791fac",
+    "url": "/css/app.2b02b018.css"
+  },
+  {
+    "revision": "35db17ae492dc3b04eff",
+    "url": "/css/chunk-vendors.ac643a66.css"
+  },
+  {
+    "revision": "e4591f6d6fe454aaeeb09006d5803208",
+    "url": "/img/W-logo.e4591f6d.png"
+  },
+  {
+    "revision": "d736f6fd69c6391c59803a7490082c41",
+    "url": "/index.html"
+  },
+  {
+    "revision": "837953fcac4d13791fac",
+    "url": "/js/app.2f74366e.js"
+  },
+  {
+    "revision": "35db17ae492dc3b04eff",
+    "url": "/js/chunk-vendors.5f935e4a.js"
+  },
+  {
+    "revision": "fb0dfd8e0a97c02feee2609ca746884c",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "3ac80f8249c5f51888ba4ed1bb24abe4",
+    "url": "/scrollbar.css"
+  }
+]);
